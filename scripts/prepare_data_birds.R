2277219bc66f78adde6775cb465d956c9e94be72
hleary <hleary.wildlife@outlook.com>
2022-10-12 09:44:18 -0700

    Start work reducing bird file size

---
+++ scripts/prepare_data_birds.R
@@ -0,0 +1,8 @@
+# Break large bird gbif data into smaller files
+# Heatherlee Leary
+# hleary.wildlife@outlook.com
+# 2022-10-12
+
+
+# Read in large bird file
+bird_data <- read.delim(file = "data/gbif/0039570-220831081235567.csv")
