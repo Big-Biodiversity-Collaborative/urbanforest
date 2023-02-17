0cde26686bee46b2910979a0b837de511c2db8e2
Jeff Oliver <jcoliver@email.arizona.edu>
2023-02-16 19:44:13 -0700

    Update GBIF query machinery (incomplete)

--- scripts/download_data.R
+++ scripts/download_data.R
@@ -5,7 +5,6 @@
 # hleary.wildlife@outlook.com
 # 2023-01-14
 
-
 # Libraries
 require(tidyverse) # Data wrangling (dyplyr package) and visualization (ggplot2 package)
 require(sf)        # Point filtering for cities
@@ -13,42 +12,39 @@ require(raster)    # Work with rasters in R (includes sp package)
 require(rgbif)     # Search and retrieve data from GBIF
 source(file = "scripts/query_gbif.R")
 
-
 # Load boundary data
 # Shapefiles obtained from City of Tucson GIS website on 2023-01-14
+# Neighborhood data are all in NAD83. GBIF data will be WGS83.
 # Tucson Neighborhoods
-neighborhood_bounds <- st_read("data/NEIGHBORHOODS_ALL.shp") # This is NAD83. GBIF data will be WGS83.
-
+neighborhood_bounds <- st_read("data/NEIGHBORHOODS_ALL.shp")
 
 # Explore data
-head(neighborhood_bounds)    #### Note that geometry doesn't have separate LAT and LONG columns ####
-class(neighborhood_bounds)   #### Why does it say "sf" and "data.frame" rather than just "data.frame"? ####
-
+# head(neighborhood_bounds)    #### Note that geometry doesn't have separate LAT and LONG columns ####
+# class(neighborhood_bounds)   #### Why does it say "sf" and "data.frame" rather than just "data.frame"? ####
 
 # Explore shapefile
-head(neighborhood_bounds)
-class(neighborhood_bounds)   # This is an "sf" type of data frame
-colnames(neighborhood_bounds)
-plot(neighborhood_bounds[,2])
+# head(neighborhood_bounds)
+# class(neighborhood_bounds)   # This is an "sf" type of data frame
+# colnames(neighborhood_bounds)
+# plot(neighborhood_bounds[,2])
 
 # Indicate whether or not to overwrite data files that already exist
-overwrite <- FALSE   
-
+overwrite <- FALSE
 
 # Identify which taxon data to pull from GBIF. 
 # Refer to GBIF for the code. Birds (aves) = 212.
 taxon_keys <- c("Aves" = 212)
 
-
 # Now download data for each neighborhood
 neighborhood_string <- paste(neighborhood_bounds$NAME)
- 
-for (i in 1:nrow(neighborhood_bounds)) {
-  neighborhood <- neighborhood_bounds$NAME[i]
-  message("downloading for ", neighborhood, ", i = ", i)
-}
 
-for (i in 1:nrow(neighborhood_bounds)) {  
+# Reality check to see that we would loop over each neighborhood 
+# for (i in 1:nrow(neighborhood_bounds)) {
+#   neighborhood <- neighborhood_bounds$NAME[i]
+#   message("downloading for ", neighborhood, ", i = ", i)
+# }
+
+for (i in 1:nrow(neighborhood_bounds)) {
   neighborhood <- neighborhood_bounds$NAME[i]
   # Make a nice name for filename, have to do it twice to avoid double underscores
   # neighborhood_name <- tolower(x = gsub(pattern = ", ",
@@ -60,60 +56,71 @@ for (i in 1:nrow(neighborhood_bounds)) {
   # neighborhood_file <- paste0("data/gbif/", neighborhood_name, "-bird-obs.csv")
   neighborhood_file <- paste0("data/gbif/neighborhood-", i, "-bird-obs.csv")
   if (overwrite | !file.exists(neighborhood_file)) {
-    message("***  Downloading data for ", neighborhood)
+    message("***  Downloading data for ", neighborhood, " (", i, " of ", length(neighborhood), ")")
     
+    # Start by re-projecting the neighborhood polygon to match the CRS of the 
+    # GBIF data
     # neighborhood_poly <- neighborhood_bounds$geometry[i]
     wgs84 <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
     neighborhood_poly <- st_transform(neighborhood_bounds$geometry[i], crs= wgs84)
     
-    # First find the maximum containing rectangle coordinates and use those for the GBIF query
+    # Find the maximum containing rectangle coordinates and use those for the GBIF query
     poly_bounds <- st_bbox(neighborhood_poly)
     min_lon <- poly_bounds["xmin"] 
     max_lon <- poly_bounds["xmax"] 
     min_lat <- poly_bounds["ymin"] 
     max_lat <- poly_bounds["ymax"] 
-    
+
+    # Do a search for observations that fall inside the smallest rectangle that 
+    # includes this neighborhood
+    rectangle_obs <- query_gbif(taxon_keys = taxon_keys,
+                                lon_limits = c(min_lon, max_lon),
+                                lat_limits = c(min_lat, max_lat),
+                                verbose = TRUE)
     
     # Bird obs are too large, so loop over years you want to search for.
-    neighborhood_obs <- NULL
-    for (year_i in 2017:2021) {
-    
-      neighborhood_year_obs <- query_gbif(taxon_keys = taxon_keys,
-                           lon_limits = c(min_lon, max_lon),
-                           lat_limits = c(min_lat, max_lat),
-                           verbose = TRUE,
-                           year_range = as.character(year_i))
-    
-    # Add this year's observations to larger data frame
-    if ("datsetName" %in% neighborhood_year_obs){
-      neighborhood_year_obs <- neighborhood_year_obs %>%
-        dplyr::select(-datasetKey)
-    }  
-    if (is.null(neighborhood_obs)){
-      neighborhood_obs <- neighborhood_year_obs
-    } else {
-      neighborhood_obs <- rbind(neighborhood_obs, neighborhood_year_obs)
+    # neighborhood_obs <- NULL
+    # for (year_i in 2017:2021) {
+    #   
+    #   neighborhood_year_obs <- query_gbif(taxon_keys = taxon_keys,
+    #                                       lon_limits = c(min_lon, max_lon),
+    #                                       lat_limits = c(min_lat, max_lat),
+    #                                       verbose = TRUE,
+    #                                       year_range = as.character(year_i))
+    #   
+    #   # Add this year's observations to larger data frame
+    #   if ("datsetName" %in% neighborhood_year_obs){
+    #     neighborhood_year_obs <- neighborhood_year_obs %>%
+    #       dplyr::select(-datasetKey)
+    #   }  
+    #   if (is.null(neighborhood_obs)){
+    #     neighborhood_obs <- neighborhood_year_obs
+    #   } else {
+    #     neighborhood_obs <- rbind(neighborhood_obs, neighborhood_year_obs)
+    #   }
+    # }
+
+    # Only proceed if at least one observation was returned for this rectangle
+    if (nrow(x = rectangle_obs)) {
+      # We have observations for the rectangle that includes this neighborhood; 
+      # next we need to exclude any observations that are not within the polygon
+      # defining the neighborhood
+      
+      # Now make the rectangle_obs into a simple feature; necessary to determine 
+      # if an individual observation is within the neighborhood polygon
+      rectangle_obs_sf <- sf::st_as_sf(x = rectangle_obs,
+                                       coords = c("decimalLongitude", "decimalLatitude"),
+                                       crs = wgs84)
+      
+      # and use it with sf::st_within; below returns logical vector indicating 
+      # whether point is within the polygon; use that vector to select rows from 
+      # rectangle_obs that are within the neighborhood polygon
+      points_within <- sf::st_within(x = rectangle_obs_sf, y = neighborhood_poly) %>% lengths > 0
+      neighborhood_obs <- rectangle_obs[points_within, ]
+      write.csv(x = neighborhood_obs,
+                file = neighborhood_file,
+                row.names = FALSE)
     }
-    }
-    
-    
-    # Convert the polygon to a simple feature for ease of filtering points
-    # neighborhood_sf <- sf::st_polygon(x = list(neighborhood_poly), dim = "XY")   
-    
-    # Now make the neighborhood_obs into a simple feature  
-    neighborhood_obs_sf <- sf::st_as_sf(x = neighborhood_obs,
-                                coords = c("decimalLongitude", "decimalLatitude"),
-                                crs = wgs84)
-    
-    # and use it with sf::st_within; below returns logical vector indicating 
-    # whether point is within the polygon; use that vector to select rows from 
-    # neighborhood_obs that are within the neighborhood polygon
-    # points_within <- sf::st_within(x = neighborhood_obs_sf, y = neighborhood_sf) %>% lengths > 0
-    points_within <- sf::st_within(x = neighborhood_obs_sf, y = neighborhood_poly) %>% lengths > 0
-    neighborhood_obs <- neighborhood_obs[points_within, ]
-    write.csv(x = neighborhood_obs,
-              file = neighborhood_file,
-              row.names = FALSE)
   }
   else {
     message("Skipping download for ", neighborhood, ", already on disk.")
--- scripts/query_gbif.R
+++ scripts/query_gbif.R
@@ -18,20 +18,18 @@
 #' returned from a call to \code{rgbif::occ_search}
 #' @param dataset_keys character vector of dataset keys to restrict results;
 #' if \code{NULL}, all datasets from query will be returned
+#' @param max_attempts integer number of maximum number of attempts for each 
+#' query to GBIF
 #' 
 #' @return data frame of observations returned from GBIF
-
-
-##### HELP: Replacing dataset_names and datasetName with dataset_keys and datasetKey. Uh... did it work?
-
 query_gbif <- function(taxon_keys, lon_limits, lat_limits, verbose = FALSE,
                        cols = c("decimalLatitude", "decimalLongitude",
                                 "individualCount", "family", "species", "year", 
-                                "month", "day", "datasetKey", "gbifID",
-                                "lifeStage"),
-                       # dataset_names = c("eBird", "iNaturalist research-grade observations"),
-                       dataset_keys = c("4fa7b334-ce0d-4e88-aaae-2e0c138d049e", "50c9509d-22c7-4a22-a47d-8c48425ef4a7"),
-                       year_range = ("2017,2021")) {
+                                "month", "day", "datasetKey", "datasetName", 
+                                "gbifID", "lifeStage"),
+                       dataset_keys = c("4fa7b334-ce0d-4e88-aaae-2e0c138d049e", 
+                                        "50c9509d-22c7-4a22-a47d-8c48425ef4a7"),
+                       year_range = ("2017,2021"), max_attempts = 5) {
   if (!require(rgbif)) {
     stop("GBIF queries require the rgbif library")
   }
@@ -39,77 +37,198 @@ query_gbif <- function(taxon_keys, lon_limits, lat_limits, verbose = FALSE,
     stop("GBIF queries require the dplyr library")
   }
   
-  # Count number of observations in the rectangle, as pagination might be 
-  # necessary; actually performs one search per taxonKey value
-  gbif_count <- rgbif::occ_search(taxonKey = taxon_keys,
-                                  limit = 1,
-                                  decimalLongitude = paste(lon_limits[1:2],
-                                                           collapse = ","),
-                                  decimalLatitude = paste(lat_limits[1:2],
-                                                          collapse = ","),
-                                    year = year_range)
-  # 
-  # For each family, get count and paginate as necessary
+  # The function rgbif::occ_search only allows multiple values for ONE 
+  # argument at a time. That is, we could pass it a vector > 1 for *either* 
+  # the taxonKey argument *OR* datasetKey, but not both. For this reason, will, 
+  # for now, iterate over values in datasetKey and send (possibly) multiple 
+  # values to taxonKey
+  
+  # List that will hold all observations, one element per datasetKey per 
+  # taxonKey
   gbif_obs_list <- list()
-  for (taxon_key in taxon_keys) {
-    # Pull out the total count of records for this taxon in this rectangle
-    # If there was only one taxon key, returned value is a result list with 
-    # five elements, but if there is more than one taxon_key, the result is a 
-    # list of lists
-    if (length(taxon_keys) > 1) {
-      taxon_count <- gbif_count[[as.character(taxon_key)]]$meta$count
-    } else {
-      taxon_count <- gbif_count$meta$count
-    }
-    if (taxon_count > 0) {
-      page <- 1
-      start <- 0
-      while(start <= taxon_count) {
+  for (datasetKey_i in 1:length(dataset_keys)) {
+    datasetKey = dataset_keys[datasetKey_i]
+    # Count number of observations in the rectangle, for this datasetKey, 
+    # as pagination might be necessary; actually performs one search per 
+    # taxonKey value
+    
+    # Using try/catch in cases of timeout
+    attempts <- 0
+    success <- FALSE
+    while (attempts < max_attempts & !success) {
+      if (attempts > 0) {
         if (verbose) {
-          #TODO: Could do better job with numbers (start at +1, check for min(start+300, taxon_count))
-          message(paste0("Downloading ", (start + 1), "-", 
-                         min((start+300), taxon_count), " of ", 
-                         taxon_count, " for taxon key ", taxon_key))
+          message("...attempt ", attempts, " failed; retrying")
         }
-        gbif_obs <- rgbif::occ_search(taxonKey = taxon_key,
-                                      decimalLongitude = paste(lon_limits[1:2],
-                                                               collapse = ","),
-                                      decimalLatitude = paste(lat_limits[1:2],
-                                                              collapse = ","),
-                                      start = start,
-                                      limit = 300,
-                                      year = year_range)
-        if (page == 1) {
-          gbif_obs_list[[as.character(taxon_key)]] <- gbif_obs$data
-        } else {
-          gbif_obs_list[[as.character(taxon_key)]] <- dplyr::bind_rows(gbif_obs_list[[as.character(taxon_key)]],
-                                                                       gbif_obs$data)
-        }
-        page <- page + 1
-        start <- (page - 1) * 300
       }
-    } else {
-      gbif_obs_list[[as.character(taxon_key)]] <- NULL
+      Sys.sleep(time = 0.5) # half-second delay between requests
+      try(expr = {
+        gbif_count <- rgbif::occ_search(taxonKey = taxon_keys,
+                                        limit = 1,
+                                        decimalLongitude = paste(lon_limits[1:2],
+                                                                 collapse = ","),
+                                        decimalLatitude = paste(lat_limits[1:2],
+                                                                collapse = ","),
+                                        year = year_range,
+                                        hasGeospatialIssue = FALSE,
+                                        datasetKey = datasetKey)
+        success <- TRUE
+      }, silent = TRUE)
+      attempts <- attempts + 1
     }
-  }
-  # We now have a list with observations for all families; combine to single 
-  # data frame
+    
+    if (attempts >= max_attempts & !success) {
+      message("...Count failed after ", attempts, " attempts; no data for this dataset.")
+    }
+    
+    if (!is.null(gbif_count)) {
+      # Figure out total number of observations (for all taxa in taxon_keys)
+      if (length(taxon_keys) > 1) {
+        # For queries of multiple taxa, returned value is series of nested lists; 
+        # need to extract the value from count from each of them
+        # Oof. Rough.
+        num_obs <- sum(unlist(lapply(X = lapply(X = gbif_count, 
+                                                FUN = "[[", "meta"),
+                                     FUN = "[[", "count")))
+      } else {
+        # If there is only one taxon_key, returned value is different (one less 
+        # level of list nesting)
+        num_obs <- gbif_count$meta$count
+      }
+      # Do a search for all observations if there was at least one observation
+      if (num_obs > 0) {
+        page <- 1
+        start <- 0
+        while(start <= num_obs) {
+          if (verbose) {
+            message(paste0("Downloading ", (start + 1), "-", 
+                           min((start+300), num_obs), " of ", num_obs))
+          }
+          
+          # Using try/catch in cases of timeout
+          attempts <- 0
+          success <- FALSE
+          while (attempts < max_attempts & !success) {
+            if (attempts > 0) {
+              if (verbose) {
+                message("...attempt ", attempts, " failed; retrying")
+              }
+            }
+            try(expr = {
+              Sys.sleep(time = 0.5) # half-second delay between requests
+              gbif_query <- rgbif::occ_search(taxonKey = taxon_keys,
+                                              decimalLongitude = paste(lon_limits[1:2],
+                                                                       collapse = ","),
+                                              decimalLatitude = paste(lat_limits[1:2],
+                                                                      collapse = ","),
+                                              start = start,
+                                              limit = 300,
+                                              year = year_range,
+                                              hasGeospatialIssue = FALSE,
+                                              datasetKey = datasetKey)
+              success <- TRUE
+            }, silent = TRUE)
+            attempts <- attempts + 1
+          }
+          
+          if (attempts >= max_attempts & !success) {
+            message("...Retrieval failed after ", attempts, " attempts; data may be incomplete.")
+          }
+          
+          # Need to add results of this query to the larger gbif_obs_list (which 
+          # has one element per datasetKey), but if there were multiple 
+          # taxon_keys passed to the search, will need to extract those data from
+          # separate list elements (there is one per taxon)
+          if (length(taxon_keys) > 1) {
+            # Extract the "data" element from each element of the query result
+            data_list <- lapply(X = gbif_query, FUN = "[[", "data")
+            gbif_obs <- dplyr::bind_rows(data_list)
+          } else {
+            # For a single taxon query, can go right to data object
+            gbif_obs <- gbif_query$data
+          }
+          
+          # Need to act differently if this is first or subsequent page
+          if (page == 1) {
+            # For first page of results, we just add the observations at the 
+            # appropriate element of the list
+            gbif_obs_list[[datasetKey]] <- gbif_obs
+          } else {
+            # All subsequent pages are bound to existing element for this 
+            # datasetKey
+            gbif_obs_list[[datasetKey]] <- dplyr::bind_rows(gbif_obs_list[[datasetKey]],
+                                                            gbif_obs)
+          }
+          page <- page + 1
+          start <- (page - 1) * 300
+        } # End while pagination
+      } # End conditional for at least one observation
+    } # End conditional for successful count
+  } # End iteration over all dataset_keys
+  
+  # We now have a list with observations for all dataset_keys; combine to 
+  # single data frame
   all_obs <- dplyr::bind_rows(gbif_obs_list)
   
-  # Apply dataset restrictions as necessary
-  if (nrow(all_obs) > 0 & length(dataset_keys) > 0) {
-    if (verbose) {
-      message(nrow(all_obs), " observations before filtering by dataset key")
-    }
-    if ("datasetKey" %in% colnames(all_obs)){
-      all_obs <- all_obs %>%
-        dplyr::filter(datasetKey %in% dataset_keys)
-      if (verbose) {
-        message(nrow(all_obs), " observations after filtering by dataset key")
-      }
-    }
+  # Select only desired columns
+  # Some results returned from GBIF do not always include all columns 
+  # (e.g. individualCount doesn't always come through); only select on 
+  # columns that *do* exist
+  if (nrow(all_obs) > 0 & length(cols) > 0) {
+    all_obs <- all_obs %>%
+      dplyr::select(dplyr::intersect(cols, colnames(all_obs)))
   }
   
+  return(all_obs)
+  
+    ########################################
+    # OLD BELOW
+    ########################################
+    
+    for (taxon_key in taxon_keys) {
+      # Pull out the total count of records for this taxon in this rectangle
+      # If there was only one taxon key, returned value is a result list with 
+      # five elements, but if there is more than one taxon_key, the result is a 
+      # list of lists
+      if (length(taxon_keys) > 1) {
+        taxon_count <- gbif_count[[as.character(taxon_key)]]$meta$count
+      } else {
+        taxon_count <- gbif_count$meta$count
+      }
+      if (taxon_count > 0) {
+        page <- 1
+        start <- 0
+        while(start <= taxon_count) {
+          if (verbose) {
+            #TODO: Could do better job with numbers (start at +1, check for min(start+300, taxon_count))
+            message(paste0("Downloading ", (start + 1), "-", 
+                           min((start+300), taxon_count), " of ", 
+                           taxon_count, " for taxon key ", taxon_key))
+          }
+          
+          gbif_obs <- rgbif::occ_search(taxonKey = taxon_key,
+                                        decimalLongitude = paste(lon_limits[1:2],
+                                                                 collapse = ","),
+                                        decimalLatitude = paste(lat_limits[1:2],
+                                                                collapse = ","),
+                                        start = start,
+                                        limit = 300,
+                                        year = year_range)
+          if (page == 1) {
+            gbif_obs_list[[as.character(taxon_key)]] <- gbif_obs$data
+          } else {
+            gbif_obs_list[[as.character(taxon_key)]] <- dplyr::bind_rows(gbif_obs_list[[as.character(taxon_key)]],
+                                                                         gbif_obs$data)
+          }
+          page <- page + 1
+          start <- (page - 1) * 300
+        }
+      } else {
+        gbif_obs_list[[as.character(taxon_key)]] <- NULL
+      }
+    }
+    
+    
   # Restrict columns as necessary
   if (nrow(all_obs) > 0 & length(cols) > 0) {
     # Some results returned from GBIF do not always include all columns 
@@ -120,5 +239,3 @@ query_gbif <- function(taxon_keys, lon_limits, lat_limits, verbose = FALSE,
   }
   return(all_obs)
 }
-
-
