314f1857dec35414c4115c3cc2430a2fdf1b3b0c
hleary <hleary.wildlife@outlook.com>
2023-04-12 07:04:20 -0700

    add shapefiles for data viz

--- scripts/urbanforest_dataset.R
+++ scripts/urbanforest_dataset.R
@@ -11,6 +11,7 @@
 require(sf)        # Point filtering
 require(raster)    # Work with rasters in R (includes sp package)
 require(tidyverse) # Manipulate data frames
+require(dplyr)
 
 # Read in data
 data <- st_read("output/shapefiles/spatial_join_data.shp")
@@ -69,7 +70,6 @@ data_df <- data_df %>%
   select(UID, neighborhood, treeEquity, speciesRich, richPerSqKm, areaSqKm, geometry)
 
 head(data_df)
-view(data_df)
 
 
 
@@ -82,9 +82,9 @@ subset_tes <- data_df[data_df$treeEquity > 0, ]
 subset_rich <- data_df[data_df$speciesRich > 0, ]
 subset_both <- data_df[data_df$treeEquity > 0 & data_df$speciesRich > 0, ]
 
-dim(subset_tes)    # 323 neighborhoods
-dim(subset_rich)   # 423 neighborhoods
-dim(subset_both)   # 296 neighborhoods
+dim(subset_tes)    # 330 neighborhoods
+dim(subset_rich)   # 424 neighborhoods
+dim(subset_both)   # 297 neighborhoods
 
 summary(subset_tes)   # range: 41.44-100.00
 summary(subset_rich)  # range: 1-262
@@ -94,8 +94,8 @@ summary(subset_both)  # treeEquity no change, speciesRich range 1-258
 # ================================================================
 # There are a total of 466 neighborhoods (includes Davis-Monthan AFB)
 # 329 have tree equity values
-# 423 have avian species richness values
-# 296 have both data
+# 424 have avian species richness values
+# 297 have both data
 # =================================================================
 
 
@@ -119,7 +119,17 @@ st_write(data_sf, dsn = "output/shapefiles/urbanforest_data.shp")
 
 
 
+# Export subsets for easy data viz later: 
 
+# Convert to spatial data type
+subset_tes_sf <- st_as_sf(subset_tes)
+subset_rich_sf <- st_as_sf(subset_rich)
+subset_both_sf <- st_as_sf(subset_both)
+
+# Save sf object as shapefile
+st_write(subset_tes_sf, dsn = "output/shapefiles/subset_tes_data.shp")
+st_write(subset_rich_sf, dsn = "output/shapefiles/subset_rich_data.shp")
+st_write(subset_both_sf, dsn = "output/shapefiles/subset_both_data.shp")
 
 
 
